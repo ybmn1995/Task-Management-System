59da2e3327709b3a6256db9e1de719a8a8cfdfa5
Yousef Basssam Nabtiti <yousef.nabtiti@atlascrisis.ae>
2025-02-15 15:49:15 +0400

    Update by ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Home from './components/Home';
 import TaskTable from './components/TaskTable';
 import CreateTaskForm from './components/CreateTaskForm';
+import TaskManager from './components/TaskManager';
 
 const App = () => {
     return (
         <div>
             <Home />
-            <CreateTaskForm />
-            <TaskTable />
+            {/* <CreateTaskForm /> */}
+            {/* <TaskTable /> */}
+            <TaskManager />
+
         </div>
     );
 };
